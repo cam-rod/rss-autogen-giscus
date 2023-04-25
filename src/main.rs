bb8c762d57df3c80991ce2fca1e5f63f9ef60bd9
Cameron Rodriguez <dev@camrod.me>
2023-04-25 14:44:26 -0400

    Add license and description

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+//! Autogenerates GitHub Discussions to be used by Giscus.
+//!
+//! This came out of a preference for a way to use Giscus, without requiring users to authenticate
+//! with the app. Since the discussion isn't created until someone comments, we needed a way to
+//! automatically create it once a blog post was uploaded.
+//!
+//! This crate checks for the latest post via the RSS feed, and then extracts the contents needed to
+//! to create a post, formatted as follows:
+//!
+//! - **Title**: URL path of the post (not including base URL)
+//! - **Description**: (potentially) First paragraph of the post, followed by a full link
+//!
+//! This crate works best when run as a GitHub Action, triggered by the completion of the
+//! `pages-build-deployment` action for GitHub pages. It depends on the RSS feed being up-to-date at the time
+//! of running, so you may need to introduce a delay.
+
 use std::time::Duration;
 
-use feed_rs::model::Link;
 use feed_rs::parser::parse;
 use octocrab::Octocrab;
 use reqwest::Client;
 
 #[tokio::main]
-async fn main() {
+pub async fn main() {
     let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env var is required");
     let octocrab = Octocrab::builder()
         .personal_token(token)
@@ -14,17 +29,16 @@ async fn main() {
         .expect("Unable to build GitHub API client");
 
     let client = reqwest::ClientBuilder::new()
-        .timeout(Duration::from_secs(30))
+        .timeout(Duration::from_secs(60))
         .build()
         .expect("Unable to build HTTP client");
 
-    let post_url  = latest_post(&client).await;
+    let post_url = latest_post(&client).await;
 
     // TODO:
     //   - Strip to the part of the URL that becomes the
     //   - get the first paragraph? or not
     //   - add link to body of request
-
 }
 
 async fn latest_post(rss_client: &Client) -> String {
@@ -37,5 +51,6 @@ async fn latest_post(rss_client: &Client) -> String {
 
     posts
         .entries.first().expect("No posts found in feed")
-        .links.first().expect("No link provided with first post").href.clone()
+        .links.first().expect("No link provided with first post")
+        .href.clone()
 }
